4498c89d1c582ee01fba15c4a9456c090fb31e6c
rummyspace <160577821+rummyspace@users.noreply.github.com>
2024-02-22 14:45:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const nocache = require("nocache");
+const path = require('path');
+const adminRoute = require('./routes/adminRoute');
+const userRoute = require('./routes/userRoute');
+
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(error => {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  });
+
+app.use(nocache());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/static',express.static(path.join(__dirname,'public')));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+ 
+app.use('/admin', adminRoute);
+app.use('/', userRoute);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
 <%- include('../partials/header')-%>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
     <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
